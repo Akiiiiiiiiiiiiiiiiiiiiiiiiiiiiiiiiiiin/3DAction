15a3675bdcaceefcfb7f3bc3f1251196dbb46729
ysaito <ysaito.yda@gmail.com>
2019-06-14 11:39:52 +0900

    [add]test

---
+++ Action3D/Source/Action3D/saiton.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ACTION3D_API saiton
+{
+public:
+	saiton();
+	~saiton();
+};
